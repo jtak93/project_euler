3e0bc8ae9954c9b1de733c1b4fa9f8128d9e6813
james tak <jtak93@gmail.com>
2017-09-08 04:31:46 -0700

    adds optimized constant time solution

--- problem_6.js
+++ problem_6.js
@@ -11,7 +11,7 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 */
-
+// Time complexity = O(n)
 function sumSquareDiff(n) {
   var sumSquare = 0;
   var squareSum = 0;
@@ -23,4 +23,22 @@ function sumSquareDiff(n) {
   return sumSquare - squareSum
 }
 
+// Time complexity = O(1) wow!
+function sumSquareDiffOptimized(n) {
+  var sum = n * (n + 1) / 2
+  var squareSum = (2 * n + 1)*(n + 1)*n/6
+  return (sum*sum) - squareSum
+}
+
+// added benchmark testing
+var t0 = Date.now()
 console.log(sumSquareDiff(100))
+var t1 = Date.now()
+var timeOne = t1-t0;
+console.log('time for sumSquareDiff', timeOne)
+var t2 = Date.now()
+
+console.log(sumSquareDiffOptimized(100))
+var t3 = Date.now()
+var timeTwo = t3-t2;
+console.log('time for sumSquareDiff', timeTwo)
