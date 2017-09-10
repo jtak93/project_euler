c4e12ef593273221322a8fc431e72f90cea2b008
james tak <jtak93@gmail.com>
2017-09-10 16:33:42 -0700

    remove comment

--- 11-20/problem_12.js
+++ 11-20/problem_12.js
@@ -17,7 +17,6 @@ We can see that 28 is the first triangle number to have over five divisors.
 What is the value of the first triangle number to have over five hundred divisors?
 */
 
-// generate triangle numbers
 function triNumWithNDiv(n) {
   var triNum = 0;
   var inc = 1;
